576af61354872463daa1d7e8701c458b5c57158f
sergeyt <stodyshev@gmail.com>
2013-12-08 18:09:17 +0700

    options

--- js/xml2json.js
+++ js/xml2json.js
@@ -4,6 +4,13 @@
  * @example var json = $.xml2json(response);
  */
 (function() {
+
+	// default options based on https://github.com/Leonidas-from-XIV/node-xml2js
+	var defaultOptions = {
+		attrkey: '$',
+		charkey: '_'
+	};
+
 	jQuery.extend({
 
 		/**w
@@ -11,13 +18,15 @@
 		 *
 		 * @param xml
 		 */
-		xml2json: function xml2json(xml) {
+		xml2json: function xml2json(xml, options) {
+			options = options || defaultOptions;
+
 			var result = {};
 
 			for (var i in xml.childNodes) {
 				var node = xml.childNodes[i];
 				if (node.nodeType === 1) {
-					var child = node.hasChildNodes() ? xml2json(node) : node.nodevalue;
+					var child = node.hasChildNodes() ? xml2json(node, options) : node.nodevalue;
 					child = child === null ? {} : child;
 
 					if (result.hasOwnProperty(node.nodeName)) {
@@ -34,17 +43,17 @@
 
 					// Add attributes if any
 					if (node.attributes.length > 0) {
-						result[node.nodeName]['@attributes'] = {};
-						child['@attributes'] = {};
+						result[node.nodeName][options.attrkey] = {};
+						child[options.attrkey] = {};
 						for (var j in node.attributes) {
 							var attribute = node.attributes.item(j);
-							child['@attributes'][attribute.nodeName] = attribute.value;
+							child[options.attrkey][attribute.nodeName] = attribute.value;
 						}
 					}
 
 					// Add element value
 					if (node.childElementCount === 0 && node.textContent !== null && node.textContent !== "") {
-						child.value = node.textContent.trim();
+						child[options.charkey] = node.textContent.trim();
 					}
 				}
 			}
